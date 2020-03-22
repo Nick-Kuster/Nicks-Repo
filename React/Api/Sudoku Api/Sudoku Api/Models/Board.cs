40c8a18bd3806b2ee70d66df299a1883b543b8c9
Nick Kuster <nkuster@verisk3e.com>
2020-03-22 09:26:22 -0400

    Cleanup
    Removed incorrect Blazor Sudoku app...
    Added Sudoku API...
    Added React Starter Template...

---
+++ React/Api/Sudoku Api/Sudoku Api/Models/Board.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sudoku_Api.Models
+{
+    public class Board
+    {
+        public List<Cell> Cells { get; set; }
+        public List<Row> Rows { get; set; }
+        public List<Column> Columns { get; set; }
+        public List<Square> Squares { get; set; }
+    }
+}
---
+++ React/Api/Sudoku Api/Sudoku Api/Models/CellContainer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sudoku_Api.Models
+{
+    public class CellContainer : GamePiece
+    {
+        public List<int> UnavailableNumbers { get; set; } = new List<int>();
+
+        public void ClearUnavailableNumbers() => this.UnavailableNumbers.Clear();
+    }
+}
